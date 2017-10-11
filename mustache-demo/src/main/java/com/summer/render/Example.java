79a2d4f8de8a8962835e84d5040852b477786547
zhougaolei <gaoleizhou@gmail.com>
2017-10-11 20:37:25 +0800

    mustache

---
+++ mustache-demo/src/main/java/com/summer/render/Example.java
@@ -0,0 +1,51 @@
+package com.summer.render;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import lombok.ToString;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Author: gaoleizhou@gmail.com
+ * Created At 2017/10/11.
+ * Desc: default
+ */
+@ToString
+public class Example {
+
+    List<Item> items() {
+        return Arrays.asList(
+                new Item("item1", "10.00", Arrays.asList(new Feature("feature1"))),
+                new Item("item2", "20.00", Arrays.asList(new Feature("feature2")))
+        );
+    }
+
+    static class Item {
+        String name, price;
+        List<Feature> features;
+        Item(String name, String price, List<Feature> features) {
+            this.name = name;
+            this.price = price;
+            this.features = features;
+        }
+    }
+
+    static class Feature {
+        String description;
+
+        Feature(String description) {
+            this.description = description;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache mustache = mf.compile("template.mustache");
+        mustache.execute(new PrintWriter(System.out), new Example()).flush();
+    }
+}
---
+++ mustache-demo/src/main/java/com/summer/render/MustacheDemoApplication.java
@@ -0,0 +1,12 @@
+package com.summer.render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheDemoApplication.class, args);
+	}
+}
